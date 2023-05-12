cb36b46aaaabf71e1ff5cab82327ad000c481e16
DogeFu <609075410@qq.com>
2023-05-12 22:52:32 +0800

    添加game管理组件，和收集系统部分组件，还有边界检测组件

---
+++ assets/module/BoundChecker.ts
@@ -0,0 +1,31 @@
+import { _decorator, Component, Node, Vec3,CCBoolean, CCInteger } from 'cc';
+import  GameManager  from './../GameManager';
+const { ccclass, property } = _decorator;
+
+@ccclass('BoundChecker')
+export class BoundChecker extends Component {
+    @property(Node)
+    target:Node = null;
+
+    @property(CCInteger)
+    bottom:number = -360;
+
+    // x：下边界
+    // y：上边界
+    @property(CCInteger)
+    top:number = 360;
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        this.target.position = new Vec3(this.target.position.x, this.target.position.y - 10,0)
+        const pos = this.target.position;
+        if (pos.y < this.bottom || pos.y > this.top) {
+            GameManager.stop()
+        }
+    }
+}
+
+
