29d7faf56b6b64ef4c6a7b0d964e028e173853b5
DogeFu <609075410@qq.com>
2023-05-13 20:30:05 +0800

    增加boss攻击逻辑

--- assets/scripts/BossAttack.ts
+++ assets/scripts/BossAttack.ts
@@ -1,6 +1,7 @@
-import { _decorator, Component, Node,Prefab,instantiate,Vec3, CCInteger } from 'cc';
+import { _decorator, Component, Node,Prefab,instantiate,Vec3,Vec2, CCInteger } from 'cc';
 const { ccclass, property } = _decorator;
 
+const radio = 720/1280;
 @ccclass('BossAttack')
 export class BossAttack extends Component {
     @property({
@@ -21,8 +22,27 @@ export class BossAttack extends Component {
     })
     maxBulletCount:number = 1;
 
+    @property({
+        type:Vec2,
+        tooltip:'子弹区间'
+    })
+    area:Vec2 = new Vec2(100,100);
+
+    @property({
+        type:CCInteger,
+        tooltip:'发射间隔（最大）'
+    })
+    timeout:number = 2000;
+
     weaponList:Node[] = [];
     handle:any;
+
+    @property({
+        type:Node,
+        tooltip:'duck节点'
+    })
+    duckNode:Node = null;
+
     start() {
 
     }
@@ -37,27 +57,33 @@ export class BossAttack extends Component {
             if (this.handle) return;
             this.handle = setTimeout(()=>{
                 const weapon = instantiate(this.bullet)
-                this.node.addChild(weapon);
+                this.node.parent.addChild(weapon);
                 // 设置随机位置
-                const y = this.getY();
-                weapon.setPosition(new Vec3(-20,y,0));// 都从右边出来
+                const pos = this.getStartPos();
+                weapon.setPosition(pos);// 都从右边出来
                 this.weaponList.push(weapon);
                 this.handle = null;
-            },Math.random()*1000)
+            },Math.random()*this.timeout)
         }
     }
 
-    getY() {
-        return Math.random() * 680 - 340;
+    getStartPos() {
+        const pos = this.node.position;
+        const x = Math.random()* this.area.x;
+        const y = Math.random()* this.area.y;
+        return new Vec3(pos.x+x,pos.y+y,0);
     }
 
     updateBullet(deltaTime: number) {
         this.weaponList.forEach((weapon)=>{
-            weapon.setPosition(new Vec3(weapon.position.x - this.speed*deltaTime,weapon.position.y,0));
+            // 往对角线方向打！
+            const offset = this.speed;
+            weapon.setPosition(new Vec3(weapon.position.x - offset,weapon.position.y - offset*radio,0));
             // 超出屏幕后销毁
-            if (weapon.position.x < - 1280/2 -10) {
+            if (weapon.position.x < - 1280/2 -10 || weapon.position.y < -720/2 -10) {
                 weapon.removeFromParent();
                 this.weaponList.splice(this.weaponList.indexOf(weapon),1);
+                console.log('移除boss子弹');
             }
         })
     }
